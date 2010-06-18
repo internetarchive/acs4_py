70c2e014bd6d9a2600df80b593ad65bc0998286b
Mike McCabe <mccabe@archive.org>
2010-06-17 18:25:12 -0700

    More progress

--- acs4.py
+++ acs4.py
@@ -2,23 +2,6 @@
 
 """
 Copyright(c)2010 Internet Archive. Software license AGPL version 3.
-
-This file is part of bookserver.
-
-    bookserver is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    bookserver is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with bookserver.  If not, see <http://www.gnu.org/licenses/>.
-
-    The bookserver source is hosted at http://github.com/internetarchive/bookserver/
 """
 
 
@@ -34,16 +17,43 @@ import base64
 import hmac
 import hashlib
 
+# from  pydbgr.api import debug
+# debug()
+
 AdeptNS = 'http://ns.adobe.com/adept'
 default_dist = 'urn:uuid:00000000-0000-0000-0000-000000000001'
+
+# Set this to a string to remove variable elements from the generated requests,
+# for debugging hmac / serialization issues.
 nonce = None
 
 """
-python ../acs4.py --password='' --debug --action=upload ia331529.us.archive.org ../logofcowboynarra00adamuoft.epub
-python ../acs4.py --password='' --debug --action=queryresourceitems ia331529.us.archive.org urn:uuid:00000000-0000-0000-0000-000000000001
+python ../acs4.py --password='' --debug upload ia331529.us.archive.org ../logofcowboynarra00adamuoft.epub
+python ../acs4.py --password='' --debug queryresourceitems ia331529.us.archive.org urn:uuid:00000000-0000-0000-0000-000000000001
+
+request --resource=(some book) --distributor=(iadistrutor) --distributionType=buy $SERVER DistributionRights create
 
 
---action=request --resource=(some book) --distributor=(iadistrutor) --distributionType=buy $SERVER DistributionRights create
+
+needed: get book info (uuid (x distributor?))
+book available?
+
+(do we get notification on book return?)
+(something that polls?)
+
+? what latency ballpark for basic book queries?
+
+mint a url - (for a particular ADE user)
+
+? how many copies available for a given book?  How to spec more?
+
+possible to keep socket open?
+
+
+
+book info
+mint a url (from book uuid)
+
 
 """
 
@@ -51,81 +61,112 @@ python ../acs4.py --password='' --debug --action=queryresourceitems ia331529.us.
 def main(argv):
     import optparse
 
-    parser = optparse.OptionParser(usage='usage: %prog [options] server_url content_filename',
-                                   version='%prog 0.1',
-                                   description='Upload a file to ACS.')
+    # borrowed from http://stackoverflow.com/questions/1857346/python-optparse-how-to-include-additional-info-in-usage-output
+    class MyParser(optparse.OptionParser):
+        def format_epilog(self, formatter):
+            return self.epilog
+
+    parser = MyParser(usage='usage: %prog [options] server_url action [arg]',
+                      version='%prog 0.1',
+                      description='Interact with ACS.',
+                      epilog="""
+python acs4.py server queryresourceitems # requires --distributor=defaultdist
+python acs4.py server upload filename
+python acs4.py server request api request_type
+""")
+
+
     parser.add_option('-p', '--password',
                       action='store',
                       help='ACS password')
     parser.add_option('-d', '--debug',
                       action='store_true',
                       help='Print debugging output')
-    parser.add_option("--action", action="store", choices=['queryresourceitems',
-                                                           'request',
-                                                           'upload'],
-                      help='ACS action to perform')
 
-    # also repeat these below, near 'dyno'
+    # also repeat these below, near 'dynamic'
     request_arg_names = ['distributor',
                          'resource',
-                         'distributionType'
+                         'distributionType',
+                         'available',
+                         'returnable',
+                         'resourceItem'
                          ]
     for name in request_arg_names:
         parser.add_option('--' + name,
-                          action='store',
-                          help=name + ' argument for request')
+                               action='store',
+                               help=name + ' argument for request')
 
     opts, args = parser.parse_args(argv)
 
-    # XXX make action naked 1rst arg
-
     if not opts.password:
-        parser.error("We think a password arg might be required")
-    if not opts.action:
-        parser.error("Please specify an action");
+        parser.error('We think a password arg might be required')
+    if len(args) < 2:
+        parser.error('Please supply at least server and action args')
 
-    if opts.action.lower() == 'queryresourceitems':
-        queryresourceitems(args[0], args[1], opts.password, opts.debug)
-    if opts.action.lower() == 'request':
-        if len(args) is not 3:
-            parser.error("For 'request' action, please supply 3 args: server, api, action")
-        if not args[2] in ['get', 'count', 'create', 'delete', 'update']:
-            parser.error('action must be get count create delete update')
-        print opts
+    server = args[0]
+    action = args[1].lower()
+
+    actions = ['queryresourceitems', 'upload', 'request']
+    if not action in actions:
+        parser.error('action arg should be one of ' + ', '.join(actions))
+        
+    if action == 'queryresourceitems':
+        queryresourceitems(server, opts.distributor, opts.password, opts.debug)
+    elif action == 'upload':
+        if len(args) != 3:
+            parser.error('For "upload" action, please supply 3 args: server, "upload", filename')
+        filename = args[2]
+        upload(server, filename, opts.password, opts.debug)
+    elif action == 'request':
+        request_types = ['get', 'count', 'create', 'delete', 'update']
+        joined = ', '.join(request_types)
+        if len(args) != 4:
+            parser.error('For "request" action, please supply server, "request", web_api, request_type - where web_api is e.g. DistributionRights, and request_type is one of ' + joined)
+        api = args[2]
+        request_type = args[3]
+        if not request_type in request_types:
+            parser.error('Request type should be one of ' + joined)
         request_args = {}
         
-        # from  pydbgr.api import debug
-        # debug()
-        
-        # TODO make this dyno, per below.  But opts is an
-        # optparse.Values, and doesn't have __getitem__!
-        # request_args = dict([(name, opts[name]) for name in request_arg_names])
-
+        # TODO make this dynamic.  But opts is an optparse.Values, and
+        # doesn't have __getitem__!  request_args = dict([(name,
+        # opts[name]) for name in request_arg_names])
         request_args['distributor'] = opts.distributor
         request_args['resource'] = opts.resource
         request_args['distributionType'] = opts.distributionType
+        request_args['available'] = opts.available
+        request_args['returnable'] = opts.returnable
+        request_args['resourceItem'] = opts.resourceItem
 
-        request(args[0], args[1], args[2], request_args, opts.password, opts.debug)
-    elif opts.action == 'upload':
-        if len(args) != 2:
-            parser.error("For 'upload' action, please supply 2 arguments: server and filename")
-        upload(args[0], args[1], opts.password, opts.debug)
+        request(server, api, request_type, request_args, opts.password, opts.debug)
     parser.destroy()
 
 
 def request(server, api, action, request_args, password, debug):
     xml = ('<?xml version="1.0" encoding="UTF-8" standalone="no"?>' +
-        '<request action="' + action + '" xmlns="http://ns.adobe.com/adept"/>')
+        '<request action="' + action + '" auth="builtin" xmlns="http://ns.adobe.com/adept"/>')
 
     print xml
     tree = etree.parse(StringIO(xml))
     root_el = tree.getroot()
     add_envelope(root_el, password, debug=debug)
-    api_el = etree.SubElement(root_el, api[0].lower() + api[1:])
+    api_el_name = api[0].lower() + api[1:]
+    if api_el_name == 'resourceItem':
+        api_el_name += 'Info'
+    if api_el_name == 'distributor':
+        api_el_name += 'Data'
+    api_el = etree.SubElement(root_el, api_el_name)
 
     for key in request_args.keys():
-        etree.SubElement(api_el, key).text = request_args[key]
-    
+        if request_args[key]:
+            etree.SubElement(api_el, key).text = request_args[key]
+
+    # perms = etree.SubElement(api_el, 'permissions')
+    # disp =  etree.SubElement(perms, 'display')
+    # etree.SubElement(disp, 'duration').text ='181'
+    # etree.SubElement(perms, 'excerpt')
+    # etree.SubElement(perms, 'print')
+
     etree.SubElement(root_el, 'hmac').text = make_hmac(password, root_el, debug)
     request = etree.tostring(tree,
                              pretty_print=True,
